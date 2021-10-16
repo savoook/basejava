e639deba1df49beec433c9a4707ddb5b0f34142b
Pavel Savin <Pavel.Savin@avon.com>
2021-10-16 11:08:09 +0300

    'Lesson_4_HW_ver1'

---
+++ src/MainReflection.java
@@ -0,0 +1,18 @@
+package com.basejava.webapp;
+
+import com.basejava.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException {
+        Resume resume = new Resume();
+        Field field = resume.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        //System.out.println(field.getName());
+        System.out.println(field.get(resume));
+        field.set(resume, "new_uuid");
+        //TODO: invoke r.toString via reflection
+        System.out.println(resume);
+    }
+}
---
+++ src/com/basejava/webapp/exception/ExistStorageException.java
@@ -0,0 +1,4 @@
+package com.basejava.webapp.exception;
+
+public class ExistStorageException {
+}
